16aeb7a8552e78de6d27fc214224edd59407b22c
AlexGiuliano20 <alex.giuliano37@gmail.com>
2022-04-19 20:56:44 -0300

    Manejo de errores

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -8,14 +8,21 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent {
   termino: string = 'Hola Mundo';
+  hayError: boolean = false;
 
   constructor(private _paisService: PaisService) {}
 
   buscar() {
+    this.hayError = false;
     console.log(this.termino);
 
-    this._paisService
-      .buscarPais(this.termino)
-      .subscribe((res) => console.log(res));
+    this._paisService.buscarPais(this.termino).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        this.hayError = true;
+      }
+    );
   }
 }
