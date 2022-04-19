c450ad8cfdfcac3b77774afa32aee3b405713da8
AlexGiuliano20 <alex.giuliano37@gmail.com>
2022-04-19 20:45:54 -0300

    Servicio para buscar países

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -8,9 +9,13 @@ import { Component } from '@angular/core';
 export class PorPaisComponent {
   termino: string = 'Hola Mundo';
 
-  constructor() {}
+  constructor(private _paisService: PaisService) {}
 
   buscar() {
     console.log(this.termino);
+
+    this._paisService
+      .buscarPais(this.termino)
+      .subscribe((res) => console.log(res));
   }
 }
